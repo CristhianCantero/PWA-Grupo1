4da8bccf62ebc5899898e85c30594e66b93b6c85
Cristhian Cantero <cristhian.cantero@init-dev.com.ar>
2022-05-05 17:56:41 -0300

    Initial Commit

---
+++ header.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title><?php echo $Titulo ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#7952b3">
+    <link rel="stylesheet" href="../../styles.css">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+</head>
+
+<body class="d-flex flex-column h-100">
+
+    <header class="mb-5">
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="../../index.php">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                TRABAJO PRACTICO 1
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio1.php">Ejercicio 1</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio2.php">Ejercicio 2</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio3.php">Ejercicio 3</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio4.php">Ejercicio 4</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio5.php">Ejercicio 5</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio6.php">Ejercicio 6</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio7.php">Ejercicio 7</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio8.php">Ejercicio 8</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio9.php">Ejercicio 9</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio10.php">Ejercicio 10</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp1/ejercicio11.php">Ejercicio Final</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                TRABAJO PRACTICO 2
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp2/ejercicio1.php">Ejercicio 1</a>
+                                </li>
+                                <li class="dropdown-item">
+                                    <a class="nav-link" href="../../trabajosPracticos/tp2/ejercicio2.php">Ejercicio 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
