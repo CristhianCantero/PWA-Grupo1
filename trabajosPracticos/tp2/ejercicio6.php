9c66e39b81ce357774ecf776b0fbb4e2490eda9c
Cristhian Cantero <cristhian.cantero@init-dev.com.ar>
2022-05-09 15:36:23 -0300

    Cambios Ejercicio 6

--- trabajosPracticos/tp2/ejercicio6.php
+++ trabajosPracticos/tp2/ejercicio6.php
@@ -17,30 +17,33 @@ include_once("../../header.php");
         </div>
     </div>
     <hr>
-</div>
-
-<div class="container mt-2">
-    <div class="container mt-2">
-        <form class="form-group mb-2 col-4" id="formularioContacto" method="POST">
-            <label class="mt-2" for="nombre">Nombre:</label>
-            <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Nombre">
-            <label class="mt-2" for="">Empresa:</label>
-            <input type="text" class="form-control" name="empresa" id="empresa" aria-describedby="helpId" placeholder="Empresa">
-            <label class="mt-2" for="telefono">Teléfono:</label>
-            <input type="number" class="form-control" name="telefono" id="telefono" aria-describedby="helpId" placeholder="Teléfono">
-            <label class="mt-2" for="mail">Mail:</label>
-            <input type="text" class="form-control" name="mail" id="mail" aria-describedby="helpId" placeholder="Mail">
-            <label class="mt-2" for="pais">Pais:</label>
+    <h2 align="center">Registro de Usuarios</h2>
+    <form class="row mb-2 col-4" id="formularioContacto" method="POST" style="margin: auto;">
+        <label class="mt-2" for="nombre">Nombre:</label>
+        <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Nombre">
+        <label class="mt-2" for="">Empresa:</label>
+        <input type="text" class="form-control" name="empresa" id="empresa" aria-describedby="helpId" placeholder="Empresa">
+        <label class="mt-2" for="telefono">Teléfono:</label>
+        <input type="number" class="form-control" name="telefono" id="telefono" aria-describedby="helpId" placeholder="Teléfono">
+        <label class="mt-2" for="mail">Mail:</label>
+        <input type="text" class="form-control" name="mail" id="mail" aria-describedby="helpId" placeholder="Mail">
+        <label class="mt-2" for="pais">Pais:</label>
+        <div class="form-group col-lg-6">
             <input type="text" id="pais" name="pais" class="form-control">
-            <select id="mostrarPaises" class="form-control mt-3">
-            </select>
-            <label class="mt-2" for="estado">Estado</label>
-            <input type="text" id="estado" name="estado" class="form-control">
-            <select id="mostrarEstados" class="form-control mt-3"></select>
-            <label class="mt-2" for="comentarios">Comentarios:</label>
-            <textarea type="text" class="form-control" name="comentarios" id="comentarios" aria-describedby="helpId" placeholder="Comentarios"></textarea>
-        </form>
-    </div>
+        </div>
+        <div class="form-group col-lg-6">
+            <select id="mostrarPaises" class="form-control"></select>
+        </div>
+        <label class="mt-2" for="estado">Estado</label>
+        <div class="form-group col-lg-6">
+            <input disabled type="text" id="estado" name="estado" class="form-control ">
+        </div>
+        <div class="form-group col-lg-6">
+            <select id="mostrarEstados" class="form-control "></select>
+        </div>
+        <label class="mt-2" for="comentarios">Comentarios:</label>
+        <textarea type="text" class="form-control" name="comentarios" id="comentarios" aria-describedby="helpId" placeholder="Comentarios"></textarea>
+    </form>
 </div>
 
 <?php
